8aded75d52d83c27fd03d1d56aef62a7d739caa1
seanheinen <seanheinen88@gmail.com>
2025-10-28 10:37:04 +0100

    Add api

---
+++ api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, {
+    snapshot: true,
+  });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap()
+  .then(() => {
+    console.log('API started');
+  })
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
